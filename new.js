4e27f464dece64e1180ec0d924541231dde3a6e7
mahtab-217 <mahtabnasiri120089@gmail.com>
2025-12-07 06:55:14 +0430

    new ones

--- new.js
+++ new.js
@@ -1,55 +1,42 @@
-const personOne={
-    id:2, name:"Ali", lastName:"Qadiri", dob:Date(), phone:"0798675849"}
-personOne.id=9;
-personOne.name="Ali Reza";
-personOne.gender="male";
-personOne.age=23;
-const listofStudents=[
-    {
-        id: 1,
-        name:"Hadi",
-        lastName:"Gholami",
-        age: 12,
-        year: new Date().getFullYear(),
-    },
-    {
-        id: 2,
-        name:"Mah",
-        lastName:"Nasiri",
-        age: 22,
-        year: new Date().getFullYear(),
-    },
-    {
-        id: 3,
-        name:"Hamid",
-        lastName:"Gholami",
-        age: 32,
-        year: new Date().getFullYear(),
-    },
-    {
-        id: 4,
-        name:"Nasa",
-        lastName:"Azad",
-        age: 28,
-        year: new Date().getFullYear(),
-    },
-    {
-        id: 5,
-        name:"Hana",
-        lastName:"Farzad",
-        age: 92,
-        year: new Date().getFullYear(),
-    },
-]
-const students=["Ali","Zia","Ahmad","Yazdan"];
-const sortedStudents=students.sort((a,b)=>{
-if(a>b){
-    return b-a;
-}
-else{
-    return a-b;
-}
+const nameList=["Yasin ","Laila","Yazdan","Naz","Ali","Frishta"];
+const sortedNames=nameList.sort((a,b)=>{
+    return b.localeCompare(a);
+});
+const numbers=["z","l","e","j","r","a"];
+const sortedNumbers=numbers.sort((a,b)=>{
+return b.localeCompare(a);
 })
-const numbers=[4,5,59,234,62,32,1,3,54]
-const sortedNumbers=numbers.sort((a,b)=>a-b);
-const indexed=students.findIndex((index)=>index==3);
+  const founded= numbers.findIndex((letter)=>{
+  return letter==="j";
+})
+
+const students=[
+{
+    id: 1,
+    name: "Muhammad",
+    lastName:"Alizada",
+    score:90
+},
+{
+    id: 2,
+    name: "Muhammad Reza",
+    lastName:"Rahmani",
+    score:99
+},
+{
+    id: 3,
+    name: "Muhammad Taha",
+    lastName:"Rahman",
+    score:99
+},
+]
+   const result=students.findIndex((student)=>{
+    return student.name==="Muhammad Reza";
+   });
+   if(result!== -1){
+    console.log("Student exists");
+
+   }
+   else{
+    console.log("Students was not found");
+   }
